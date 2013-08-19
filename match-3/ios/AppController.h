369870b07ed20f827ba6d5aea17df7a66d1724fa
Vlad <vlad@MacBook-Pro-Vladislav.local>
2013-08-19 16:44:05 +0700

    Added cocos2d-x template files

---
+++ match-3/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  match_3AppController.h
+//  match-3
+//
+//  Created by Vlad on 19.08.13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ match-3/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  match_3AppController.h
+//  match-3
+//
+//  Created by Vlad on 19.08.13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
